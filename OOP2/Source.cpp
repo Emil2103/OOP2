3dcf66472e9f2b684d3a77ba1a819fda7e0efa9a
Emil Saubanov <emilsaubanov2013@gmail.com>
2020-11-19 23:09:24 +0500

    продолжаю работу

--- OOP2/Source.cpp
+++ OOP2/Source.cpp
@@ -3,36 +3,50 @@
 #include <conio.h>
 using namespace std;
 
-class Nationality {
+class People {
 private:
-	int x;
-	int y;
+	string name, surname, Say;
 public:
-	Nationality() {
-		printf("Nationality()\n");
-		x = 0;
-		y = 0;
-
+	People() {
+		printf("People()\n");
 	}
-	Nationality(int x, int y) {
-		printf("Nationality(int x, int y)\n");
-		this->x = x;
-		this->y = y;
+	People(string name, string surname) {
+		printf("People(int name)\n");
+		this->name = name;
+		this->surname = surname;
 	}
-	Nationality(const Nationality &n) {
-		printf("Nationality(const Point &p)\n");
-		x = n.x;
-		y = n.y;
+	People(const People &n) {
+		printf("People(const Point &p)\n");
+		name = n.name;
+		surname = n.surname;
 	}
-	~Nationality() {
-		cout << x << ' ' << y << endl;
+	~People() {
+		cout << name << " " << surname << endl;
 		printf("~Nationality()\n");
+	}
+	void Change_surname(string surn) {
+		surname = surn;
 	}
 };
 
 int main() {
-	
-
+	{
+		People Man1;
+		People Man2("Egor", "Vakbilash");
+		People Man3(Man2);
+	}
+	{
+		cout << endl;
+		People* p1 = new People;
+		People* p2 = new People("Aleksandr", "Pupkin");
+		People* p3 = new People(*p2);
+		People* chel = new People("Gena", "Zalupkin");
+		chel->Change_surname("Chelovecheskay");
+		delete p1;
+		delete p2;
+		delete p3;
+		delete chel;
+	}
 	
 
 }
