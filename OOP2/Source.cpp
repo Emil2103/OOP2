136cd3d9b46ea464dfab159c22ef1276798e2019
Emil Saubanov <emilsaubanov2013@gmail.com>
2020-11-20 00:11:59 +0500

    скоро закончу

--- OOP2/Source.cpp
+++ OOP2/Source.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 
 class People {
-private:
+protected:
 	string name, surname, Say;
 public:
 	People() {
 		printf("People()\n");
 	}
 	People(string name, string surname) {
-		printf("People(int name)\n");
+		printf("People(string name, string surname)\n");
 		this->name = name;
 		this->surname = surname;
 	}
 	People(const People &n) {
-		printf("People(const Point &p)\n");
+		printf("People(const Point &n)\n");
 		name = n.name;
 		surname = n.surname;
 	}
-	~People() {
+	virtual ~People() {
 		cout << name << " " << surname << endl;
 		printf("~Nationality()\n");
 	}
@@ -29,14 +29,46 @@ public:
 	}
 };
 
+class Nationality : public People {
+private:
+	string nation;
+public:
+	Nationality(): People() {
+		printf("Nationality()\n");
+	}
+	Nationality(string name, string surname, string nation):People(name,surname) {
+		printf("Nationality(string name, string surname, string nation)\n");
+		this->nation = nation;
+	}
+	Nationality(const Nationality& n) {
+		printf("People(const Point &n)\n");
+		name = n.name;
+		surname = n.surname;
+		nation = n.nation;
+	}
+	~Nationality() {
+		cout << name << " " << surname << " " << nation << endl;
+		printf("~Nationality()\n");
+	}
+	void sound() {
+		if(nation == "Tatar")
+			cout << "TATAR say: исэмесес" << endl;
+	}
+	void eat();
+};
+void Nationality:: eat(){
+	cout << "Съел ущпущмак на завтрак " << endl;
+}
+
 int main() {
+	setlocale(LC_ALL, "rus");
 	{
 		People Man1;
 		People Man2("Egor", "Vakbilash");
 		People Man3(Man2);
 	}
 	{
-		cout << endl;
+		cout << "-------------------" << endl;
 		People* p1 = new People;
 		People* p2 = new People("Aleksandr", "Pupkin");
 		People* p3 = new People(*p2);
@@ -46,7 +78,24 @@ int main() {
 		delete p2;
 		delete p3;
 		delete chel;
+		cout << "-------------------" << endl;
 	}
-	
+	{
+		cout << "-------------------" << endl;
+		Nationality *N = new Nationality("Mamduh", "Baizigitov", "Tatar");
+		Nationality* Q = new Nationality("Niyaz", "Fazlyev", "Bashkir");
+		Q->sound();
+		N->sound();
+		N->eat();
+		delete N;
+		delete Q;
+		cout << "-------------------" << endl;
+	}
+	{
+		cout << "-------------------" << endl;
+		People* Q = new Nationality("Niyaz", "Fazlyev", "Bashkir");
+		delete Q;
+		cout << "-------------------" << endl;
+	}
 
 }
